c44700d75ede5386f171326392e0ca93f3fcae96
Cezar Takada <cezar.takada@integ.cl>
2024-07-19 15:25:17 -0400

    Refactor index.d.ts to simplify interface naming
    The interface `DMSParamsCoords` was renamed to `DMSCoords` and its corresponding usages were updated in the exporting functions for simplicity and consistency. Furthermore, the `DMSResponseCoords` interface was removed to make the code more maintainable by reducing redundancy. Now, both input parameters and response objects share the same `DMSCoords` interface.

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ declare module 'geo-coord-utils' {
         zoneNum: number;
         zoneLetter: string;
     }
-    interface DMSParamsCoords {
+    interface DMSCoords {
         latD: number,
         latM: number,
         latS: number,
@@ -16,20 +16,15 @@ declare module 'geo-coord-utils' {
         lonDir: string
     }
 
-    interface DMSResponseCoords {
-        lat: string;
-        lon: string;
-    }
-
     interface LatLonCoords {
         latitude: number;
         longitude: number;
     }
 
-    export function UTMToDMS(params:UTMCoords): DMSResponseCoords;
+    export function UTMToDMS(params:UTMCoords): DMSCoords;
     export function UTMToLatLon(params:UTMCoords): LatLonCoords;
     export function LatLonToUTM(lat: number, lon: number): UTMCoords;
-    export function LatLonToDMS(lat: number, lon: number): DMSResponseCoords;
-    export function DMSToLatLon(params:DMSParamsCoords): LatLonCoords;
-    export function DMSToUTM(params:DMSParamsCoords): UTMCoords;
+    export function LatLonToDMS(lat: number, lon: number): DMSCoords;
+    export function DMSToLatLon(params:DMSCoords): LatLonCoords;
+    export function DMSToUTM(params:DMSCoords): UTMCoords;
 }
